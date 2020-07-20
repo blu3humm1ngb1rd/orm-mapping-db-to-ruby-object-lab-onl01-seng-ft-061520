9cf67bd3e1b09011b03df51c53dd4622d9b54b3f
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 02:12:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student
       WHERE name = ?
       LIMIT 1
     SQL
-    DB[:conn].execute(sql, name).map do |name| 
+    DB[:conn].execute(sql, name).map do |row| 
       
     end 
   end
