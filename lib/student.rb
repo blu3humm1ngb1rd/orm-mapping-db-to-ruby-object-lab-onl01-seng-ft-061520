d8146e772d20ef0ae3f2e8e599649d2a9a0b1e0e
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 02:50:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,11 +100,11 @@ class Student
     end.first 
   end 
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * 
       FROM students
-      WHERE students.grade = 10 
+      WHERE students.grade = grade 
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
