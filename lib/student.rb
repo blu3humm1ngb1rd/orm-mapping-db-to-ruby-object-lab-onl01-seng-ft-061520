61507d1668dded7683f59e37fffc5b88814c8f2c
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 02:34:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,8 +73,6 @@ class Student
       FROM students
       WHERE grade < 12 
     SQL
-    DB[:conn].execute(sql, name).map do |row| 
-      self.new_from_db(row)
-    end.first  
+    DB[:conn].execute(sql)
   end 
 end
